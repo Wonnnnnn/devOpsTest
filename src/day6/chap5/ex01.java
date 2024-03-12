0821afe2395033ab35e4c6e81d43d72edb728844
Wonnnnnn <01022549653@naver.com>
2024-03-12 11:30:39 +0900

    version 2.0

---
+++ src/day6/chap5/ex01.java
@@ -0,0 +1,7 @@
+package day6.chap5;
+
+public class ex01 {
+  public static void main(String[] args) {
+    System.out.println("test");
+  }
+}
---
+++ src/day6/chap5/ex02.java
@@ -0,0 +1,7 @@
+package day6.chap5;
+
+public class ex02 {
+  public static void main(String[] args) {
+    System.out.println("happy!");
+  }
+}
